dfc40b745e73fe499d333d66a75c158ab9ea545a
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-05-28 23:10:30 +0530

    package update

--- server.js
+++ server.js
@@ -49,6 +49,10 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+
 app.use("*", (req, res) => {
   res.status(StatusCodes.NOT_FOUND).json({ msg: "not found !" });
 });
